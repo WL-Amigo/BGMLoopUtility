397c06b23345635a43fbe96ecd41311b8df7d214
Amigo(WhiteLuckers) <white.luckers@gmail.com>
2017-01-18 17:12:41 +0900

    add WaveFormFileType (enum of file type)

---
+++ BGMLoopUtility/includes/WaveFormFileType.hpp
@@ -0,0 +1,11 @@
+#ifndef WAVEFORMFILETYPE_HPP
+#define WAVEFORMFILETYPE_HPP
+
+enum class WaveFormFileType {
+    RIFFWAVE,
+    OGGVORBIS,
+    AAC,
+    UNKNOWN
+};
+
+#endif // WAVEFORMFILETYPE_HPP
