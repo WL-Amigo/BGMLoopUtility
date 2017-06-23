d4a757824d1fa1f8db3d9e9aff97b28960903cac
Amigo(WhiteLuckers) <white.luckers@gmail.com>
2017-06-23 23:27:12 +0900

    Modify: zoom-outed waveform drawing method

--- BGMLoopUtility/includes/WaveFormViewerBase.hpp
+++ BGMLoopUtility/includes/WaveFormViewerBase.hpp
@@ -2,26 +2,32 @@
 #define WAVEFORMVIEWERBASE_HPP
 
 #include <QColor>
+#include <QVector>
 #include <QWidget>
 #include "includes/WaveFormData.hpp"
 
 struct SamplePair {
-    qint16 l;
-    qint16 r;
+    qint16 lMax;
+    qint16 rMax;
+    qint16 lMin;
+    qint16 rMin;
 };
 
 class WaveFormViewerBase : public QWidget {
     Q_OBJECT
 public:
     explicit WaveFormViewerBase(QWidget* parent = nullptr);
+    void setRatioExponential(quint8 ratioExp);
+    quint8 getRatioExponential();
 
 protected:
-    void drawWaveForm(WaveFormData& data, quint32 center, quint8 ratioExp,
-                      QColor color);
-    SamplePair getDrawSampleHeight(WaveFormData& data, quint32 drawSamplePos,
-                                   quint8 ratioExp);
+    void drawWaveForm(WaveFormData& data, quint32 center, QColor color);
+    SamplePair getDrawSampleHeight(WaveFormData& data, quint32 drawSamplePos);
 
 private:
+    quint8 m_ratioExp;
+    QVector<SamplePair> m_spCache;
+
 signals:
 
 public slots:
--- BGMLoopUtility/src/WaveFormViewerBase.cpp
+++ BGMLoopUtility/src/WaveFormViewerBase.cpp
@@ -2,12 +2,21 @@
 #include <QPainter>
 #include <QtMath>
 
-static const qint16 MAX_INT16 = 32767;
+static const qint32 MAX_UINT16 = 65535;
+static const qint32 MAX_INT16 = 32767;
 
-WaveFormViewerBase::WaveFormViewerBase(QWidget* parent) : QWidget(parent) {}
+WaveFormViewerBase::WaveFormViewerBase(QWidget* parent)
+    : QWidget(parent), m_ratioExp(0), m_spCache() {}
+
+void WaveFormViewerBase::setRatioExponential(quint8 ratioExp) {
+    this->m_ratioExp = ratioExp;
+    this->m_spCache.clear();
+}
+
+quint8 WaveFormViewerBase::getRatioExponential() { return this->m_ratioExp; }
 
 void WaveFormViewerBase::drawWaveForm(WaveFormData& data, quint32 center,
-                                      quint8 ratioExp, QColor color) {
+                                      QColor color) {
     QPainter painter(this);
     auto width = this->width();
     auto height = this->height();
@@ -22,57 +31,82 @@ void WaveFormViewerBase::drawWaveForm(WaveFormData& data, quint32 center,
                        : 0;
          idx < width; idx++) {
         qint32 drawSamplePosition = static_cast<int>(center) - harfWidth + idx;
-        auto samplePair =
-            this->getDrawSampleHeight(data, drawSamplePosition, ratioExp);
+        auto samplePair = this->getDrawSampleHeight(data, drawSamplePosition);
 
         // draw L sample
-        int sampleHeight = qRound(static_cast<qreal>(qAbs(samplePair.l)) /
-                                  static_cast<qreal>(MAX_INT16) *
-                                  static_cast<qreal>(quarterHeight));
-        int y =
-            (samplePair.l < 0) ? quarterHeight - sampleHeight : quarterHeight;
+        int yMax = qRound(static_cast<qreal>(samplePair.lMax) /
+                          static_cast<qreal>(MAX_UINT16) *
+                          static_cast<qreal>(quarterHeight));
+        int yMin = qRound(static_cast<qreal>(samplePair.lMin) /
+                          static_cast<qreal>(MAX_UINT16) *
+                          static_cast<qreal>(quarterHeight));
+        int sampleHeight = yMax - yMin;
+        int y = quarterHeight - yMax;
         painter.fillRect(idx, y, 1, sampleHeight, color);
 
         // draw R sample
-        sampleHeight = qRound(static_cast<qreal>(qAbs(samplePair.l)) /
-                              static_cast<qreal>(MAX_INT16) *
-                              static_cast<qreal>(quarterHeight));
-        y = (samplePair.r < 0) ? quarterHeight - sampleHeight : quarterHeight;
+        yMax = qRound(static_cast<qreal>(samplePair.rMax) /
+                      static_cast<qreal>(MAX_UINT16) *
+                      static_cast<qreal>(quarterHeight));
+        yMin = qRound(static_cast<qreal>(samplePair.rMin) /
+                      static_cast<qreal>(MAX_UINT16) *
+                      static_cast<qreal>(quarterHeight));
+        sampleHeight = yMax - yMin;
+        y = quarterHeight - yMax;
         y += harfHeight;
         painter.fillRect(idx, y, 1, sampleHeight, color);
     }
 }
 
 SamplePair WaveFormViewerBase::getDrawSampleHeight(WaveFormData& data,
-                                                   quint32 drawSamplePos,
-                                                   quint8 ratioExp) {
+                                                   quint32 drawSamplePos) {
     quint32 ratio = 1;
-    for (quint8 t = 1; t < ratioExp; t++) {
+    for (quint8 t = 0; t < this->m_ratioExp; t++) {
         ratio *= 2;
     }
     quint32 actualSamplePosition = drawSamplePos * ratio;
 
-    // if ratio == 1, return raw value
-    if (ratio == 1) {
+    // if ratio == 0, return raw value
+    if (ratio == 0) {
+        auto lValue = data.lChannel.at(actualSamplePosition);
+        auto rValue = data.rChannel.at(actualSamplePosition);
         if (actualSamplePosition <= data.length()) {
-            return {data.lChannel.at(actualSamplePosition),
-                    data.rChannel.at(actualSamplePosition)};
+            return {lValue >= 0 ? lValue : static_cast<qint16>(0),
+                    rValue >= 0 ? rValue : static_cast<qint16>(0),
+                    lValue < 0 ? lValue : static_cast<qint16>(0),
+                    rValue < 0 ? rValue : static_cast<qint16>(0)};
         } else {  // if position is out of bound, return 0
-            return {0, 0};
+            return {0, 0, 0, 0};
         }
     }
 
-    // calcurate average from actualSamplePosition to
+    // if cache is available, return value from cache
+    if (this->m_spCache.length() > drawSamplePos &&
+        this->m_spCache.at(drawSamplePos).lMax >= 0)
+        return this->m_spCache.at(drawSamplePos);
+
+    // calcurate max/min from actualSamplePosition to
     // actualSamplePosition + ratio - 1
-    qint32 sumL = 0;
-    qint32 sumR = 0;
+    SamplePair result = {0, 0, 0, 0};
     for (quint32 idx = 0; idx < ratio; idx++) {
         if (idx + actualSamplePosition >= data.length()) break;
-        sumL += data.lChannel.at(idx + actualSamplePosition);
-        sumR += data.rChannel.at(idx + actualSamplePosition);
+        auto lValue = data.lChannel.at(idx + actualSamplePosition);
+        auto rValue = data.rChannel.at(idx + actualSamplePosition);
+        if (result.lMax < lValue) result.lMax = lValue;
+        if (result.rMax < rValue) result.rMax = rValue;
+        if (result.lMin > lValue) result.lMin = lValue;
+        if (result.rMin > rValue) result.rMin = rValue;
     }
-    sumL /= ratio;
-    sumR /= ratio;
 
-    return {static_cast<qint16>(sumL), static_cast<qint16>(sumR)};
+    // save result to cache
+    while (this->m_spCache.length() < drawSamplePos) {
+        this->m_spCache.push_back({-1, -1, 1, 1});
+    }
+    if (this->m_spCache.length() == drawSamplePos) {
+        this->m_spCache.push_back(result);
+    } else {  // length > drawSamplePos
+        this->m_spCache[drawSamplePos] = result;
+    }
+
+    return result;
 }
